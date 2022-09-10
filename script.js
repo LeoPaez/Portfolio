e033ba9409dde7eae72c78de04d5352329dd3991
LeoPaez <lpaezbusiness@gmail.com>
2022-09-10 11:45:34 -0300

    Se suben cambios finales

--- script.js
+++ script.js
@@ -59,3 +59,5 @@ window.addEventListener("load", ()=> {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
     }
 })
+
+document.querySelectorAll('.special').forEach(l=>l.addEventListener('click',e => document.getElementById('openSidebarMenu').click()));
